e436a74977c7496c343988ab1f2e079c509acbc7
Teo <teomacedo@gmail.com>
2019-09-19 10:33:04 -0400

    reajustes links

--- app/Http/Controllers/Web/ControllerSite.php
+++ app/Http/Controllers/Web/ControllerSite.php
@@ -14,20 +14,16 @@ class ControllerSite extends Controller
     public function link($link) {
 
         
-        if ($link == 'whatsApp-01'){
-            return redirect('https://api.whatsapp.com/send?1=pt_BR&phone=5566996029513');
+        if ($link == 'whatsApp'){
+            return redirect('https://api.whatsapp.com/send?1=pt_BR&phone=5566997219347');
         }
         
-        if ($link == 'whatsApp-02'){
-            return redirect('https://api.whatsapp.com/send?1=pt_BR&phone=5566984020566');
-        }
-
-        if ($link == 'teste-gratis'){
+        if ($link == 'menuvem-cadastro'){
             return redirect('https://sys.nuctecnologia.com.br/cadastro');
         }
 
         else {
-            return view('welcome');
+            return redirect('https://nuctecnologia.com.br/blog');
         }
         
     }
